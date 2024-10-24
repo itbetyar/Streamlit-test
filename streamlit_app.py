b2734986fefc54bab731c33f530801c44d9fea6d
IT Betyár <145707654+itbetyar@users.noreply.github.com>
2024-10-25 01:59:18 +0200

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+# 2024.10.20 - Ben - Python | main
+
+import streamlit as st
+
+st.title("IT Betyár Streamlit minta")
+
+st.write("Sima write elem")
+
+st.image("kep.jpg", caption="Valami", width=150)
+
+st.audio("zene.mp3")
